1cba0f2c84fd1c69694b8379c5d2860542655268
baharriyan <indonesiariyan@gmail.com>
2020-10-20 18:56:46 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN7-integer/src/integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class integer {
+   
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+    short s;
+    int i;
+    long l;
+    b = 120;
+    s = 32767;
+    i = 1_000_000_000;
+     l = 9000000000L;
+        
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
